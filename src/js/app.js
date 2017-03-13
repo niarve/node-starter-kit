a4549d8a1eeca2e239b7ddc179ae9854a45f5a9a
Nathan Iarve <NIarve@manta.com>
2017-03-13 00:35:10 -0500

    angular dependency added and functional

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,10 @@ console.log("hello nate");
 
 var entry = require('./entry.js');
 entry();
+
+var angular = require('angular');
+
+var app = angular.module('app', []);
+app.controller('mainController', ['$scope', ($scope) => {
+  $scope.greeting =  {text: 'Hello Angular' };
+}]);
